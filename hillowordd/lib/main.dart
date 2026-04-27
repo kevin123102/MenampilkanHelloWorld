4da057fbce0ea5c7c28e1288a929f0ad7ae7612a
kevin123102 <ibusayang246@gmail.com>
2026-04-27 22:40:46 +0700

    update semua

--- hillowordd/lib/main.dart
+++ hillowordd/lib/main.dart
@@ -1,16 +1,54 @@
 import 'package:flutter/material.dart';
-import 'package:hillowordd/hello_word.dart';
-import 'package:hillowordd/column_widget.dart';
-import 'package:hillowordd/row_widget.dart';
-import 'package:hillowordd/ui/produk_form.dart';
+import 'package:hillowordd/page/beranda_page.dart';
+import 'package:hillowordd/page/profile_page.dart';
+import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<Widget> _page = [BerandaPage(), ProfilePage()];
+
+  int currentPage = 0;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Menampilkan hello World', home: Produkform());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ), // ThemeData
+      home: Scaffold(
+        body: _page[currentPage],
+        bottomNavigationBar: SalomonBottomBar(
+          currentIndex: currentPage,
+          onTap: (i) => setState(() => currentPage = i),
+          items: [
+            //Beranda
+            SalomonBottomBarItem(
+              icon: Icon(Icons.home),
+              title: Text("Beranda"),
+              selectedColor: Colors.blue,
+            ), // SalomonBottomBarItem
+
+            //Profile
+            SalomonBottomBarItem(
+              icon: Icon(Icons.person),
+              title: Text("Profile"),
+              selectedColor: Colors.blue,
+            ), // SalomonBottomBarItem
+          ],
+        ), // SalomonBottomBar
+      ), // Scaffold
+    ); // MaterialApp
   }
 }
